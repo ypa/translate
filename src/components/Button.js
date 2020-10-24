19466eca876a0e13a106c7ebb06c57c099994d72
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-24 08:39:30 -0700

    Wire up contextType and provider so that non-direct child components can access data in the grandparent's state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === 'english' ? 'Submit' : 'နှိပ်';
+
+    return <button className="ui button primary">{text}</button>;
   }
 }
 

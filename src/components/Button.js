e8b5e958ede081ac8bc5b11f90ea6fe18a426b53
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-25 08:39:26 -0700

    Refactor Button to use context consumer, which is another way in addition to contextType + this.context approach, to get the data out of context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,19 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'နှိပ်';
+  }
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'နှိပ်';
-
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 

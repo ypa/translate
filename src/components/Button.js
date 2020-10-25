52864db9607ad7fbf744d80c8caed27ed664ae18
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-25 08:54:42 -0700

    Add ColorContext to demostrate how we can consume multiple contexts from a single component by using context consumers

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
 
@@ -7,13 +8,21 @@ class Button extends React.Component {
     return value === 'english' ? 'Submit' : 'နှိပ်';
   }
 
-  render() {
+  renderButton(color) {
     return (
-      <button className="ui button primary">
+      <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
           {(value) => this.renderSubmit(value)}
         </LanguageContext.Consumer>
       </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        { (color) => this.renderButton(color) }
+      </ColorContext.Consumer>
     );
   }
 }

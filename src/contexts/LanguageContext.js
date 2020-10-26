d679a1b88d179cd206ec60d61956fa842e566f2c
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-26 07:42:55 -0700

    Implement LanguageStore so that we can separate out business logic inside there

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,23 @@
 import React from 'react';
 
-export default React.createContext('english');
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState( { language });
+  }
+
+  render() {
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+        // Wrapping the children components with the context provider so that
+        // they all can access the context object passsed in value prop.
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
